f58042d2f99d688b72546797087666fe5b5203dc
KW <kw@KWnoMacBook-Pro.local>
2021-08-13 18:01:41 +0900

    削除機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.all
   end
@@ -28,6 +28,10 @@ class PostsController < ApplicationController
     else
       render :edit
     end
+  end
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice:"投稿を削除しました！"
   end
   private
   def set_post
