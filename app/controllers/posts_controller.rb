a996ef3bacacabd66e8cb10f18a73039045b2d36
KW <kw@KWnoMacBook-Pro.local>
2021-08-13 14:39:00 +0900

    コントローラにStrong Parametersを記述

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,14 @@ class PostsController < ApplicationController
   def index
   end
   def new
+    @post = Post.new
+  end
+  def create
+    Post.create(params.require(:post).permit(:title, :content))
+    redirect_to new_post_path
+  end
+  private
+  def post_params
+    params.require(:blog).permit( :content)
   end
 end
