05346becf4d70f9dab7813a61bcdfa07ffcd2305
KW <kw@KWnoMacBook-Pro.local>
2021-08-13 16:43:35 +0900

    エラーメッセージの表示機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,14 @@ class PostsController < ApplicationController
     Post.create(params.require(:post).permit(:title, :content))
     redirect_to new_post_path
   end
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, notice: "投稿しました！"
+    else
+      render :new
+    end
+  end
   def show
     @post = Post.find(params[:id])
   end
